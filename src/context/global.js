ea66b47464f8699aa4f8861d8a496c5be2d42887
chandra-12-sinha <chandrakalasinha083@gmail.com>
2024-09-12 23:20:29 +0530

    added context api in the project

---
+++ src/context/global.js
@@ -0,0 +1,17 @@
+import { createContext, useContext } from "react";
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({children}) =>{
+
+    return(
+        <GlobalContext.Provider value={'hello'}>
+
+            {children}
+        </GlobalContext.Provider>
+    )
+};
+
+export const useGlobalContext = ()=>{
+    return useContext(GlobalContext);
+}
